af66f6b212ef6b5ed7c4757802efa230b1e620d5
emilien-dublet <emilien.dublet@gmail.com>
2018-05-13 21:33:09 +0200

    i18n

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+	'name' => Yii::t('app','Project Manager'),
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
